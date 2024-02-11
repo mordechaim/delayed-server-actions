b186d85465319d5fe8166d0b4c72207661e0522f
Mordechai Meisels <mordechai@codaworks.com>
2024-02-11 15:32:20 -0500

    Complete reproduction

---
+++ src/app/page/page.js
@@ -0,0 +1,7 @@
+import Link from 'next/link';
+
+export default function Page() {
+    return <div className='p-10'>
+        <Link href='/' className='text-blue-500 underline'>Back home</Link>
+    </div>
+}
